8eb740746655680b6cdba3548663e4296347be4a
Brad Stalcup <brad@alacritylinux.(none)>
2013-05-14 16:34:38 -0400

    added frames n stuff

--- spritesheet.lua
+++ spritesheet.lua
@@ -37,3 +37,55 @@ function SpriteSheet:update(dt)
 		sprite:update(dt)
 	end
 end
+
+
+
+function loadSS()
+	local ss = SpriteSheet:new()
+	ss:addFrames("grass",{
+			default={
+				frames = {
+						love.graphics.newQuad( 0, 0, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 0, 32, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 0, 64, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 0, 96, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 0, 128, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 0, 160, 32, 32, 1024, 1024 )
+					},
+				target = "default"
+			}
+			--snow{
+			--	frames = {Quads},
+			--	target = "snow"
+			--}
+
+		}
+		)
+	ss:addFrames("water",{
+			default={
+				frames = {
+						love.graphics.newQuad( 32, 0, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 32, 32, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 32, 64, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 32, 96, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 32, 128, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 32, 160, 32, 32, 1024, 1024 )
+					},
+				target = "default"
+				}
+			})
+	ss:addFrames("block", {
+			default={
+				frames = {
+						love.graphics.newQuad( 64, 0, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 64, 32, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 64, 64, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 64, 96, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 64, 128, 32, 32, 1024, 1024 )
+						love.graphics.newQuad( 64, 160, 32, 32, 1024, 1024 )
+
+				}
+			}
+		})
+end
+		
